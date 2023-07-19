ab32cfdacc525b628ef18061bda3658ef2b8d269
abbaymi <32568907+abbaymi@users.noreply.github.com>
2023-07-19 13:39:36 -0400

    created sign up, form input and button components, updated firebase utils to handle sign up with email and password

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,14 +1,33 @@
-import { signInWithGooglePopup, createUserDocumentFromAuth } from "../../utils/firebase/firebase.utils";
+import { useEffect  } from "react";
+import { getRedirectResult } from "firebase/auth";
+
+import { signInWithGooglePopup, signInWithGoogleRedirect, createUserDocumentFromAuth, auth } from "../../utils/firebase/firebase.utils";
+import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
 
 const SignIn = () => {
+    useEffect(() => {
+        async function _getRedirectResult() {
+            const response = await getRedirectResult(auth);
+
+            if (response) {
+                const userDocRef = await createUserDocumentFromAuth(response.user);
+            }
+        }
+
+        _getRedirectResult();
+    }, []);
+
     const logGoogleUser = async () => {
         const { user } = await signInWithGooglePopup();
         const userDocRef = await createUserDocumentFromAuth(user);
     } 
+
     return (
         <div>
             <h1> Sign in works</h1>
             <button onClick={logGoogleUser}>Sign in with Google Popup</button>
+            <button onClick={signInWithGoogleRedirect}>Sign in with Google Redirect</button>
+            <SignUpForm />
         </div>
     );
 }
