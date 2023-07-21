dbd31083abc9ec8d9e8d3d850285592fd0d46316
abbaymi <32568907+abbaymi@users.noreply.github.com>
2023-07-21 15:14:44 -0400

    minor refacroring and clean up

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -2,6 +2,7 @@ import { useParams } from 'react-router-dom';
 import { useContext, useEffect, useState } from 'react';
 import { CategoriesContext } from '../../contexts/categories.context';
 import ProductCard from '../../components/product-card/product-card.component';
+import { CategoryContainer } from './category.styles';
 
 const Category = () => {
     const { category } = useParams();
@@ -13,14 +14,14 @@ const Category = () => {
     }, [category, categoriesMap]);
 
     return (
-        <div className='category-container'>
+        <CategoryContainer>
             <h2 className='title'>{category.toUpperCase()}</h2>
             <div className="body">
                 {
                     products && products.map((product) => <ProductCard key={product.id} product={product} />)
                 }
             </div>
-        </div>
+        </CategoryContainer>
     )
 }
 
