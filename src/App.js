1b19765cf2e01695ead8e8be563dcf2168511545
abbaymi <32568907+abbaymi@users.noreply.github.com>
2023-07-21 10:30:24 -0400

    implemented checkout page including checkout items and functionality

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import Navigation from './routes/navigation/navigation.component';
 import { Suspense, lazy } from 'react';
 import Shop from './routes/shop/shop.component';
+import Checkout from './routes/checkout/checkout.component';
 
 const Home = lazy(() => import('./routes/home/home.component'));
 const Authentication = lazy(() => import('./routes/authentication/authentication.component'));
@@ -14,6 +15,7 @@ const App = () => {
           <Route index element={<Suspense fallback={'Loading...'}><Home/></Suspense>} />
           <Route path='auth' element={<Suspense fallback={'Loading...'}><Authentication /></Suspense>}></Route>
           <Route path='shop' element={<Suspense fallback={'Loading...'}><Shop /></Suspense>}></Route>
+          <Route path='checkout' element={<Suspense fallback={'Loading...'}><Checkout /></Suspense>}></Route>
       </Route>
     </Routes>
   );
